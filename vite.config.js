f7cd7b605e6dafa8aa966e1d5b4cb2666a35e215
Amelia Froemming <afroemming@wisc.edu>
2025-07-18 19:12:37 -0500

    correct group number

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  base: '/p0/',
+  base: '/p44/',
   build: {
     outDir: 'docs'
   }
